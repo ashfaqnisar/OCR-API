83a649d34e407686f2aab2cbf4f644bc19e8a90e
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-05-13 22:22:59 +0530

    Updated the sentry dependency

--- server/app.js
+++ server/app.js
@@ -13,8 +13,8 @@ const app = express();
 Sentry.init({ dsn: 'https://f2c1250fc2344eaa8c11e9a3e2503fb9@o361783.ingest.sentry.io/5239445' });
 // app.set('views', path.join(__dirname, 'routes'));
 // app.set('view engine', 'pug');
-app.use(cors());
 app.use(Sentry.Handlers.requestHandler());
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -35,10 +35,10 @@ app.get('/debug-sentry', function mainHandler(req, res) {
 
 app.use(Sentry.Handlers.errorHandler());
 
-// app.use(function onError(err, req, res, next) {
-//     res.statusCode = 500;
-//     res.end(res.sentry + "\n" + err.message);
-// });
+app.use(function onError(err, req, res, next) {
+    res.statusCode = 500;
+    res.end(res.sentry + "\n" + err.message);
+});
 
 const listener = app.listen(process.env.PORT || 8080 || 8500, function () {
     console.log("Listening on port " + listener.address().port);
