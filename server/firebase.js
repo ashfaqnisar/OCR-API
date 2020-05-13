3c187495cb716117cf32f40bc9d71f5a04f73d5a
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-05-13 22:32:26 +0530

    Added the firebase to the project

--- server/firebase.js
+++ server/firebase.js
@@ -1,15 +1,6 @@
 import firebase from "firebase";
+import firebaseConfig from './firebase.json'
 
-const config = {
-  apiKey: "AIzaSyDn1kAPpSLARPrSBb83SZruTaC6qcCH49g",
-  authDomain: "ims-invoice-management-system.firebaseapp.com",
-  databaseURL: "https://ims-invoice-management-system.firebaseio.com",
-  projectId: "ims-invoice-management-system",
-  storageBucket: "ims-invoice-management-system.appspot.com",
-  messagingSenderId: "82002035448",
-  appId: "1:82002035448:web:7b4e1e352e5b69fe68b10d"
-};
-
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
 export default firebase;
