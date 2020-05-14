6018e5f05ec89586e0d60b93cd227b4f9817a446
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-05-14 19:25:35 +0530

    Solved the error with the firebase json

--- server/firebase.js
+++ server/firebase.js
@@ -1,6 +1,15 @@
 import firebase from "firebase";
-import firebaseConfig from './firebase.json'
+const config ={
+    "apiKey": "AIzaSyAM5ll_76WioPQyUqole9R_cT923t8X4Og",
+    "authDomain": "ezerka-ocr.firebaseapp.com",
+    "databaseURL": "https://ezerka-ocr.firebaseio.com",
+    "projectId": "ezerka-ocr",
+    "storageBucket": "ezerka-ocr.appspot.com",
+    "messagingSenderId": "736297238373",
+    "appId": "1:736297238373:web:6c3dbe34c87b6e1ba4eb8c",
+    "measurementId": "G-T9PG0MJEQN"
+}
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 export const db = firebase.firestore();
 export default firebase;
