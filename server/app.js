2f5a6e3953418fbb12f1e72f80ecb9e5ee604f10
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-05-15 11:53:48 +0530

    Updated the api to process pdf at the google cloud file

--- server/app.js
+++ server/app.js
@@ -2,15 +2,13 @@ import express from 'express';
 import path from 'path';
 import morgan from 'morgan';
 import cors from 'cors';
-import indexRouter from './routes/index'
-import usersRouter from './routes/users'
-import firebase, {db} from './firebase'
 import * as Sentry from '@sentry/node';
+import vision from '@google-cloud/vision'
 
 const app = express();
 
 
-Sentry.init({ dsn: 'https://f2c1250fc2344eaa8c11e9a3e2503fb9@o361783.ingest.sentry.io/5239445' });
+Sentry.init({dsn: 'https://f2c1250fc2344eaa8c11e9a3e2503fb9@o361783.ingest.sentry.io/5239445'});
 // app.set('views', path.join(__dirname, 'routes'));
 // app.set('view engine', 'pug');
 app.use(Sentry.Handlers.requestHandler());
@@ -20,11 +18,58 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
 
+const client = new vision.ImageAnnotatorClient()
+
+const bucketName = "bucket-ezerka-ocr"
+
+const fileName = "sample.pdf"
+
+const outputPrefix = 'results'
+
+const gcsSourceUri = `gs://${bucketName}/${fileName}`;
+const gcsDestinationUri = `gs://${bucketName}/${outputPrefix}/`;
+
+const inputConfig = {
+    // Supported mime_types are: 'application/pdf' and 'image/tiff'
+    mimeType: 'application/pdf',
+    gcsSource: {
+        uri: gcsSourceUri,
+    },
+};
+const outputConfig = {
+    gcsDestination: {
+        uri: gcsDestinationUri,
+    },
+};
+const features = [{type: 'DOCUMENT_TEXT_DETECTION'}];
+const request = {
+    requests: [
+        {
+            inputConfig: inputConfig,
+            features: features,
+            outputConfig: outputConfig,
+        },
+    ],
+};
 
 
+app.get('/', async (req, res) => {
+    try {
+        const [operation] = await client.asyncBatchAnnotateFiles(request);
+        const [filesResponse] = await operation.promise();
+        const destinationUri =
+            filesResponse.responses[0].outputConfig.gcsDestination.uri;
+        console.log('Json saved to: ' + destinationUri);
 
-app.get('/', (req, res) => {
-    res.status(200).send(`OCR API`)
+        res.status(200).send(destinationUri)
+    } catch (e) {
+        const err = {
+            code: e.code || 500,
+            message: e.message || e.status
+        }
+        res.status(e.code || 500).send(err)
+    }
+
 });
 
 
