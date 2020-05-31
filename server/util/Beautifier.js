a769d5ef272d812d5787eca5b99ed0ac0aa7a20c
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-05-31 17:44:17 +0530

    Updated the api response

--- server/util/Beautifier.js
+++ server/util/Beautifier.js
@@ -1,12 +1,10 @@
 export const processResponse = (response) => {
     let data = {};
-    data["input"] = response.result[0].input
-    data["filepath"] = response.result[0].filepath
+    data["uploadedFile"] = response.result[0].input
     data["prediction"] = {}
     response.result[0].prediction.map((prediction) => {
         data["prediction"] = {[prediction.label]: {text: prediction.ocr_text}, ...data["prediction"]}
     })
-    data["filename"] = response.result[0].filepath.split("/")[3].split(".")[0]
+    data["fileId"] = response.result[0].filepath.split("/")[3].split(".")[0]
     return data
-
 }
