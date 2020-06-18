ac55db86ee8fbd569e5b544d109de0f0eb613c0e
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-18 11:28:21 +0530

    Updated the sample response from ocr

--- server/response.js
+++ server/response.js
@@ -6,7 +6,115 @@ export default {
             "input": "test2.pdf",
             "prediction": [
                 {
-                    "label": "firstName",
+                    "label": "providerHealthCare",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": 'Treejoy HealthCare'
+                },
+                {
+                    "label": "providerName",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": 'Vamshi'
+                },
+                {
+                    "label": "NPI",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "1417960428"
+                },
+                {
+                    "label": "providerAddress",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "Gachibowli"
+                },
+                {
+                    "label": "providerCity",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "Hyderabad"
+                },
+                {
+                    "label": "providerState",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "Telangana"
+                },
+                {
+                    "label": "providerZip",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "500006ē"
+                },
+                {
+                    "label": "providerNumber",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "8328277518"
+                },
+                {
+                    "label": "providerFaxNumber",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "1234567890"
+                },
+                {
+                    "label": "orderIcdCode",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "z12"
+                },
+                {
+                    "label": "orderDateOfOrder",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "05/19/1999"
+                },
+                {
+                    "label": "patientId",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "12345"
+                },
+                {
+                    "label": "patientFirstName",
                     "xmin": 895,
                     "ymin": 846,
                     "xmax": 1021,
@@ -15,14 +123,241 @@ export default {
                     "ocr_text": "Ashfaq"
                 },
                 {
-                    "label": "secondName",
+                    "label": "patientSecondName",
                     "xmin": 895,
                     "ymin": 846,
                     "xmax": 1021,
                     "ymax": 879,
                     "score": 1,
                     "ocr_text": "Nisar"
-                }
+                },
+                {
+                    "label": "patientDOB",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "05/19/1999"
+                },
+                {
+                    "label": "patientSex",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "Male"
+                },
+                {
+                    "label": "patientNumber",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "8328277518"
+                },
+                {
+                    "label": "patientLanguage",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "English"
+                },
+                {
+                    "label": "patientShippingAddress",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "Gachibowli"
+                },
+                {
+                    "label": "patientShippingAddressCity",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "Hyderabad"
+                },
+                {
+                    "label": "patientShippingAddressState",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "Telangana"
+                },
+                {
+                    "label": "patientShippingAddressZip",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": 500006
+                },
+                {
+                    "label": "patientBillingAddress",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "Gachibowli"
+                },
+
+                {
+                    "label": "patientBillingAddressCity",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "Hyderabad"
+                },
+                {
+                    "label": "patientBillingAddressState",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": "Telangana"
+                },
+                {
+                    "label": "patientBillingAddressZip",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": 500006
+                },
+                {
+                    "label": "patientIsHispanicLatinoOrigin",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": false
+                },
+                {
+                    "label": "patientRace",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": 'Asian'
+                },
+                {
+                    "label": "billingIsESInsurance",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": false
+                },
+                {
+                    "label": "policyHolderName",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": 'Ashfaq'
+                },
+                {
+                    "label": "policyHolderDOB",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": '05/19/1999'
+                },
+                {
+                    "label": "policyHolderRelationshipToPatient",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": 'self'
+                },
+                {
+                    "label": "primaryInsuranceCarrier",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": 'Bajaj'
+                },
+                {
+                    "label": "billingType",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": 'Ashfaq'
+                },
+                {
+                    "label": "billingClaimsSubmissionAddress",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": 'Second Floor, Bajaj Insurance, Hi Tech city, Hyderabad'
+                },
+                {
+                    "label": "billingPolicyNumber",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": '12345'
+                },
+                {
+                    "label": "billingGroupNumber",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": '12345'
+                },
+                {
+                    "label": "billingPlan",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": 'platinum'
+                },
+                {
+                    "label": "billingPriorAuthorizationCode",
+                    "xmin": 895,
+                    "ymin": 846,
+                    "xmax": 1021,
+                    "ymax": 879,
+                    "score": 1,
+                    "ocr_text": ''
+                },
+
             ],
             "page": 0,
             "request_file_id": "135d6b79-08b1-40b8-9143-42ab2b221871",
