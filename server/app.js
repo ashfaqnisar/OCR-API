2a4c8d2603f1db2c64e54efe9ea3d99429787ab3
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-28 14:38:50 +0530

    Added the delete endpoints

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ import {Storage} from '@google-cloud/storage';
 import multer from "multer";
 import response from './response';
 import axios from 'axios'
-import {processResponse} from "./util/Beautifier";
+import {beautifyResponse, processResponse} from "./util/Beautifier";
 import fs from 'fs';
 import {promisify} from 'util'
 import FormData from 'form-data'
@@ -44,6 +44,7 @@ const storage = new Storage({
 });
 
 const increment = firebase.firestore.FieldValue.increment(1);
+const decrement = firebase.firestore.FieldValue.increment(-1);
 
 
 const bucketName = "esocr-app"
@@ -142,6 +143,32 @@ app.get("/users/:uid", async (req, res) => {
     }
 
 });
+app.delete("/users/:uid", async (req, res) => {
+    try {
+        const {uid} = req.params;
+
+        const userRef = db.collection("users").doc(uid.toString())
+        const statsRef = db.collection("--stats--").doc("users");
+
+        const batch = db.batch();
+
+        batch.delete(userRef)
+        batch.set(statsRef, {count: decrement})
+
+        batch.commit()
+            .then(() => res.status(200).send(`Deleted ${uid} successfully`))
+
+
+    } catch (err) {
+        const error = {
+            code: err.code || 500,
+            message: err.message || err.status,
+        }
+        res.status(err.code || 500).json(error);
+    }
+
+});
+
 app.get("/users/:uid/stats", async (req, res) => {
     try {
         const {uid} = req.params;
@@ -265,6 +292,37 @@ app.post("/ocr", mul.single("file"), async (req, res, next) => {
         res.status(err.code || 500).json(error);
     }
 })
+app.get("/ocr", async (req, res) => {
+    try {
+        const {uid} = req.query;
+        if (!uid) {
+            res.status(400).json({code: 400, message: "Please,provide the uid with the request"})
+            return
+        }
+        const user = await db.collection('users').doc(uid.toString()).get()
+        if (!user.exists) {
+            res.status(400).json({code: 400, message: `No, user present with the uid ${uid}`})
+            return
+        }
+        let ocrArray;
+
+        const ocrDocs = await db.collection("users").doc(uid).collection("ocr")
+            .orderBy("processedAt", "desc")
+            .get()
+
+        ocrArray = ocrDocs.docs.map(ocrDoc => ocrDoc.data())
+
+        res.status(200).send(ocrArray);
+
+    } catch (err) {
+        const error = {
+            code: err.code || 500,
+            message: err.message || err.status,
+        }
+        res.status(err.code || 500).json(error);
+    }
+
+});
 app.get("/ocr/:ocrId", async (req, res) => {
     try {
         const {ocrId} = req.params;
@@ -291,37 +349,6 @@ app.get("/ocr/:ocrId", async (req, res) => {
             return
         }
         res.status(200).send(ocr.data());
-
-    } catch (err) {
-        const error = {
-            code: err.code || 500,
-            message: err.message || err.status,
-        }
-        res.status(err.code || 500).json(error);
-    }
-
-});
-app.get("/ocr", async (req, res) => {
-    try {
-        const {uid} = req.query;
-        if (!uid) {
-            res.status(400).json({code: 400, message: "Please,provide the uid with the request"})
-            return
-        }
-        const user = await db.collection('users').doc(uid.toString()).get()
-        if (!user.exists) {
-            res.status(400).json({code: 400, message: `No, user present with the uid ${uid}`})
-            return
-        }
-        let ocrArray;
-
-        const ocrDocs = await db.collection("users").doc(uid).collection("ocr")
-            .orderBy("processedAt", "desc")
-            .get()
-
-        ocrArray = ocrDocs.docs.map(ocrDoc => ocrDoc.data())
-
-        res.status(200).send(ocrArray);
 
     } catch (err) {
         const error = {
@@ -374,6 +401,44 @@ app.put("/ocr/:ocrId", async (req, res) => {
     }
 
 });
+app.delete("/ocr/:ocrId", async (req, res) => {
+    try {
+        const {ocrId} = req.params;
+        const {uid} = req.query;
+
+        if (!(ocrId && uid)) {
+            res.status(400).json({code: 400, message: "Please,provide the uid & ocrId"})
+            return
+        }
+        const user = await db.collection('users').doc(uid.toString()).get()
+        if (!user.exists) {
+            res.status(400).json({code: 400, message: `No, user present with the uid ${uid}`})
+            return
+        }
+
+        const ocrRef = db.collection("users").doc(uid).collection("ocr").doc(ocrId)
+        const statsRef = db.collection("--stats--").doc("ocr");
+        const userOCRStatsRef = db.collection("users").doc(uid.toString()).collection('info').doc("ocr");
+
+        const batch = db.batch();
+
+        batch.delete(ocrRef)
+        batch.set(userOCRStatsRef, {count: decrement}, {merge: true});
+        batch.set(statsRef, {count: decrement}, {merge: true});
+        batch.commit()
+            .then(() => res.status(200).send(`Deleted ${ocrId} successfully`))
+
+    } catch (err) {
+        const error = {
+            code: err.code || 500,
+            message: err.message || err.status,
+        }
+        res.status(err.code || 500).json(error);
+    }
+
+});
+
+
 app.post("/ocr/raw", mul.single("file"), async (req, res) => {
     try {
         if (!req.file) {
@@ -397,6 +462,40 @@ app.post("/ocr/raw", mul.single("file"), async (req, res) => {
             ocrResponse = nanonetsResponse.data
         }
 
+        res.status(200).json(ocrResponse)
+
+    } catch (err) {
+        console.log(err)
+        const error = {
+            code: err.code || 500,
+            message: err.message || err.status,
+        }
+        res.status(err.code || 500).json(error);
+    }
+})
+app.post("/ocr/raw/beautify", mul.single("file"), async (req, res) => {
+    try {
+        if (!req.file) {
+            res.status(400).json({code: 400, message: 'Please, provide an file with the request '})
+            return
+        }
+        let ocrResponse = response
+
+        if (useNanonets) {
+            const formData = new FormData()
+            formData.append('modelId', '4ed6dcd3-d1e4-424d-9780-e4acfde58c78')
+            formData.append('file', fs.createReadStream(req.file.path))
+
+            const nanonetsResponse = await axios.post('https://app.nanonets.com/api/v2/OCR/Model/4ed6dcd3-d1e4-424d-9780-e4acfde58c78/LabelFile/',
+                formData, {
+                    headers: {
+                        ...formData.getHeaders(),
+                        'Authorization': 'Basic ' + Buffer.from('-w7X4B2isVUQ1BRAFbuypR8lED41DlD5' + ':').toString('base64')
+                    }
+                })
+            ocrResponse = nanonetsResponse.data
+        }
+        ocrResponse = beautifyResponse(ocrResponse)
         res.status(200).json(ocrResponse)
 
     } catch (err) {
