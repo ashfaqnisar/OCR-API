2646f9b7c7f3233a1c098139fb508c37e863b77a
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-06-30 06:15:47 +0530

    Fixed the issue with timeout in heroku

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import path from 'path';
 import morgan from 'morgan';
 import cors from 'cors';
 import * as Sentry from '@sentry/node';
+import timeout from 'connect-timeout';
 
 import users from './routes/users'
 import ocr from './routes/ocr'
@@ -23,6 +24,7 @@ Sentry.init({
 });
 
 app.use(Sentry.Handlers.requestHandler());
+app.use(timeout('60s'))
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
